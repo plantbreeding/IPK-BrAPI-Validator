151329ae22ad17c574e7e6e2ed374f860c6c8de1
Martín Basterrechea <martinbaste@gmail.com>
2017-11-23 09:30:24 +0100

    get last n reports

--- src/main/java/de/ipk_gatersleben/bit/bi/bridge/brapicomp/dbentities/TestReportService.java
+++ src/main/java/de/ipk_gatersleben/bit/bi/bridge/brapicomp/dbentities/TestReportService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.UUID;
 
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.stmt.QueryBuilder;
 
 import de.ipk_gatersleben.bit.bi.bridge.brapicomp.testing.reports.TestSuiteReport;
 import de.ipk_gatersleben.bit.bi.bridge.brapicomp.utils.DataSourceManager;
@@ -41,6 +42,18 @@ public class TestReportService {
         return testReportDao.queryForId(UUID.fromString(reportId));
     }
 
-    //Todo: Get latest n reports for endpoint.
-
+	/**
+	 * Get the last n reports for a given endpoint in ascending order (oldest first)
+	 * @param endpoint
+	 * @param last Number of reports to get.
+	 * @return latest endpoints
+	 * @throws SQLException 
+	 */
+	public static List<TestReport> getLastReports(Endpoint endpoint, long last) throws SQLException {
+		Dao<TestReport, UUID> testReportDao = DataSourceManager.getDao(TestReport.class);
+		QueryBuilder<TestReport, UUID> qb = testReportDao.queryBuilder();
+		qb.where().eq(TestReport.ENDPOINTID_FIELD_NAME, endpoint.getId().toString());
+		qb.orderBy(TestReport.DATE_FIELD_NAME, true).limit(last); //Ascending
+		return qb.query();
+	}
 }
