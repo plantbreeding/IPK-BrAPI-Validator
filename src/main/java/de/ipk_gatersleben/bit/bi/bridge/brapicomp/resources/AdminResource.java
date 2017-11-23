f7246e567ef4f652998973edbb8a894b699ca9bd
Martín Basterrechea <martinbaste@gmail.com>
2017-11-23 15:54:44 +0100

    switch weekly to monday at 8

--- src/main/java/de/ipk_gatersleben/bit/bi/bridge/brapicomp/resources/AdminResource.java
+++ src/main/java/de/ipk_gatersleben/bit/bi/bridge/brapicomp/resources/AdminResource.java
@@ -14,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import de.ipk_gatersleben.bit.bi.bridge.brapicomp.dbentities.Endpoint;
 import de.ipk_gatersleben.bit.bi.bridge.brapicomp.dbentities.TestReport;
 import de.ipk_gatersleben.bit.bi.bridge.brapicomp.utils.DataSourceManager;
 import org.glassfish.jersey.process.internal.RequestScoped;
