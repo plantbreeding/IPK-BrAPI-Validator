5f6d5f605e5535d2666513cd41c7ba1a1f51de21
Guy Davenport <guy.davenport@partners.basf.com>
2023-04-10 19:36:45 +1200

    fixed spring versions, added jpa profile and set as default

--- brava-app/jpa/src/main/java/org/brapi/brava/jpa/JPAConfig.java
+++ brava-app/jpa/src/main/java/org/brapi/brava/jpa/JPAConfig.java
@@ -2,8 +2,10 @@ package org.brapi.brava.jpa;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@Profile("jpa")
 @Configuration
 @EnableJpaRepositories
 @EntityScan
